274ee2d2e38d22f832d2eb9b21366b01a55a780d
Daniel Deren <Daniel@ddMacBook-Pro.local>
2018-08-25 11:38:19 +0200

    Utils lib re-written

--- utils.js
+++ utils.js
@@ -1,62 +1,66 @@
 (function () {
-    window.utils = {
-        loaded: true,
-        getStatus: function (statusCode) {
-            switch (statusCode) {
-                case "ACTION_INCOMPATIBLE_WITH_SOURCE":
-                    return { status: statusCode, message: "This action is incompatible with the given source." };
-                case "ACTION_INCOMPATIBLE_WITH_SOURCE":
-                    return { status: statusCode, message: "This action is incompatible with the given source." };
-                case "ACTION_INCOMPATIBLE_WITH_SOURCE":	
-                    return { status: statusCode, message: "This action is incompatible with the given source." };
-                case "SITE_ERROR":	
-                    return { status: statusCode, message: "A site error occurred while trying to perform this action." };
-                case "USER_LOGGED_OUT":	
-                    return { status: statusCode, message: "User is not logged in, for an unknown reason." };
-                case "ORDER_NUMBER_NOT_EXIST":	
-                    return { status: statusCode, message: "Order with provided number does not exist." };
-                case "INVALID_URL":	
-                    return { status: statusCode, message: "URL is invalid." };
-                case "INVALID_QUANTITY_PROVIDED":	
-                    return { status: statusCode, message: "Incorrect quantity Value." };
-                case "PRODUCT_DOES_NOT_EXIST":	
-                    return { status: statusCode, message: "Product not found." };
-                case "PRODUCT_OUT_OF_STOCK":	
-                    return { status: statusCode, message: "This product is not available at this time." };
-                case "PRODUCT_REQUIRES_ADDITIONAL_DETAILS":	
-                    return { status: statusCode, message: "Product requires additional details to add to cart." };
-                case "QUANTITY_EXCEEDS_AVAILABLE_STOCK":	
-                    return { status: statusCode, message: "The quantity provided exceeded the actual quantity in stock." };
-                case "QUANTITY_EXCEEDS_MAX_ALLOWED":	
-                    return { status: statusCode, message: "The quantity provided would exceed the maximum  quantity allowed in cart." };
-                case "LOGIN_DELIVERY_ZIP_MISMATCH":	
-                    return { status: statusCode, message: "Zip provided during login is different than the zip used in shipping address. This is not allowed." };
-                case "MAX_SHIPPING_COST_EXCEEDED":	
-                    return { status: statusCode, message: "Actual shipping cost exceeded the maximum specified." };
-                case "MAX_SHIPPING_DAYS_EXCEEDED":	
-                    return { status: statusCode, message: "Actual shipping days exceeded the maximum specified." };
-                case "MAX_TOTAL_EXCEEDED":	
-                    return { status: statusCode, message: "Order total exceeded the maximum specified." };
-                case "CC_MISMATCH":	
-                    return { status: statusCode, message: "Credit card provided was not found or no default credit card exists." };
-                case "CVV_REQUIRED":	
-                    return { status: statusCode, message: "CVV code was not provided in the input." };
-                case "CVV_INCORRECT":	
-                    return { status: statusCode, message: "Provided CVV code is incorrect." };
-                case "CART_EMPTY":	
-                    return { status: statusCode, message: "Shopping cart contains no items." };
-                case "PRODUCT_NOT_AVAILABLE_FOR_SHIPPING":	
-                    return { status: statusCode, message: "Products in cart are not available for shipping." };
-                case "ADDRESS_MISSING":	
-                    return { status: statusCode, message: "Provided address doesn't exist in the list of shipping addresses." };
-                case "BUY_ACTION_FAILED":	
-                    return { status: statusCode, message: "Failed to finalize the order." };
-                case "REQUIRES_CREDIT_CARD_RE-ENTRY":
-                    return { status: statusCode, message: "Credit card details are required to be re-entered during the final checkout steps. We cannot proceed at this time." };
-                case "ACTION_NOT_SUPPORTED":	
-                    return { status: statusCode, message: "This action is not compatible with this source." };
-                default:
-                    return { status: "UNKNOWN", message: "Status not recognized by utils lib." };
+    let _jsActionContext;
+    let _status = {
+        AUTH_SUCCESS: { auth_status: "SUCESS", auth_message: "" },               
+        AUTH_2FA: { auth_status: "2FA", auth_message: "" },               
+        AUTH_UNKNOWN: { auth_status: "UNKNOWN", auth_message: "" },               
+        ACTION_INCOMPATIBLE_WITH_SOURCE: { status: "FAILED", message: "This action is incompatible with the given source." },
+        SITE_ERROR: { status: "FAILED", message: "A site error occurred while trying to perform this action." },
+        USER_LOGGED_OUT: { status: "FAILED", message: "User is not logged in, for an unknown reason." },
+        ORDER_NUMBER_NOT_EXIST: { status: "FAILED", message: "Order with provided number does not exist." },
+        INVALID_URL: { status: "FAILED", message: "URL is invalid." },
+        INVALID_QUANTITY_PROVIDED: { status: "FAILED", message: "Incorrect quantity Value." },
+        PRODUCT_DOES_NOT_EXIST: { status: "FAILED", message: "Product not found." },
+        PRODUCT_OUT_OF_STOCK: { status: "FAILED", message: "This product is not available at this time." },
+        PRODUCT_REQUIRES_ADDITIONAL_DETAILS: { status: "FAILED", message: "Product requires additional details to add to cart." },
+        QUANTITY_EXCEEDS_AVAILABLE_STOCK: { status: "FAILED", message: "The quantity provided exceeded the actual quantity in stock." },
+        QUANTITY_EXCEEDS_MAX_ALLOWED: { status: "FAILED", message: "The quantity provided would exceed the maximum  quantity allowed in cart." },
+        LOGIN_DELIVERY_ZIP_MISMATCH: { status: "FAILED", message: "Zip provided during login is different than the zip used in shipping address. This is not allowed." },
+        MAX_SHIPPING_COST_EXCEEDED: { status: "FAILED", message: "Actual shipping cost exceeded the maximum specified." },
+        MAX_SHIPPING_DAYS_EXCEEDED: { status: "FAILED", message: "Actual shipping days exceeded the maximum specified." },
+        MAX_TOTAL_EXCEEDED: { status: "FAILED", message: "Order total exceeded the maximum specified." },
+        CC_MISMATCH: { status: "FAILED", message: "Credit card provided was not found or no default credit card exists." },
+        CVV_REQUIRED: { status: "FAILED", message: "CVV code was not provided in the input." },
+        CVV_INCORRECT: { status: "FAILED", message: "Provided CVV code is incorrect." },
+        CART_EMPTY: { status: "FAILED", message: "Shopping cart contains no items." },
+        PRODUCT_NOT_AVAILABLE_FOR_SHIPPING: { status: "FAILED", message: "Products in cart are not available for shipping." },
+        ADDRESS_MISSING: { status: "FAILED", message: "Provided address doesn't exist in the list of shipping addresses." },
+        BUY_ACTION_FAILED: { status: "FAILED", message: "Failed to finalize the order." },
+        REQUIRES_CREDIT_CARD_RE: { status: "FAILED", message: "Credit card details are required to be re-entered during the final checkout steps. We cannot proceed at this time." },
+        ACTION_NOT_SUPPORTED: { status: "FAILED", message: "This action is not compatible with this source." }
+    }
+
+    window.__Utils = class Utils {
+        constructor(jsActionContext) {   
+            _jsActionContext = jsActionContext;               
+            this.endEx = function(authObjId, statusObjId, authFailedMsg) {
+                let returnData;
+    
+                if (!authFailedMsg) {         
+                    let authObj = _status[authObjId] || {};
+                    let statusObj = _status[statusObjId] || {};
+    
+                    if ((!authObjId || typeof authObjId != "string") && (!statusObjId || typeof statusObjId != "string")) {
+                        return console.log("No authObjId and no statusObjId provided.");                   
+                    } else if (!_status[authObjId] && !_status[authObjId]) {
+                        return console.log("authObjId and statusObjId does not math any known status.");
+                    }
+                    
+                    returnData = Object.assign(authObj, statusObj)
+                } else {
+                    returnData = {
+                        auth_status: "FAILED",
+                        auth_message: authFailedMsg
+                    };
+                }
+    
+                this.memory.returnData = returnData;
+                console.log(JSON.stringify(returnData)); 
+                this.return(this.createData(returnData));
+            }.bind(_jsActionContext);
+
+            if (_jsActionContext.memory && _jsActionContext.memory.returnData) {
+                _jsActionContext.return(_jsActionContext.createData(_jsActionContext.memory.returnData));
             }
         }
     }
