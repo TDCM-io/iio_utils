9b74a14b304ec43dd6bd3e5781e5e3b39aa56834
Wojciech Nowakowski <wojciechnowakowski9@gmail.com>
2018-08-27 16:10:05 +0200

    Fix checkTimeframes function
    Changed key values to strings.

--- utils.js
+++ utils.js
@@ -65,9 +65,9 @@
             this.checkTimeframes = function(timeframes) {
               let returnData;
               if (timeframes.length != 2) {
-                return _endEx(_status.AUTH_SUCCESS, _status.INVALID_TIMEFRAME_QUANTITY);
+                return _endEx('AUTH_SUCCESS', 'INVALID_TIMEFRAME_QUANTITY');
               } else if (!/^\d{10}$/.test(timeframes[0]) || !/^\d{10}$/.test(timeframes[1]) || (timeframes[1] > parseInt(new Date().getTime()/1000))) {
-                return _endEx(_status.AUTH_SUCCESS, _status.INVALID_TIMEFRAME);
+                return _endEx('AUTH_SUCCESS', 'INVALID_TIMEFRAME');
               }
               return true;
             }.bind(_jsActionContext);
