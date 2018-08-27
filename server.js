dc17c9b00d6beffe2c86e24e5b732f2e8ca45576
Daniel Deren <Daniel@ddMacBook-Pro.local>
2018-08-27 14:19:20 +0200

    Port taken from Heroku env or 9000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const port = process.argv[2] || 80;
+const port = process.env.PORT || 9000;
 
 http.createServer(function (req, res) {
   console.log(`${req.method} ${req.url}`);
