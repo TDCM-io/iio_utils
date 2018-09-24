7c8bcaa5dfd0821ea0aec0f0c89211b59c22f6ed
Madnarth <corwyn.blade@gmail.com>
2018-09-24 10:43:17 +0200

    Up

--- CodeActionBuilder/buildCode.js
+++ CodeActionBuilder/buildCode.js
@@ -13,45 +13,8 @@ const siteUrl = dir.config.siteUrl;
 
 const interactions = `module.exports = ${dir.interactions.toString()}`;
 
-<<<<<<< HEAD
-var jsonText = JSON.stringify({
-  'extractionConfigs': {},
-  'interactions': [{
-      'constructor': 'GotoAction',
-      'url': siteUrl,
-      "options": {
-        "externalScripts": [
-          "https://tdcmioiio.herokuapp.com/utils.js",
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
-        ]
-      }
-    },
-    {
-      'constructor': 'CodeAction',
-      'code': interactions,
-    },
-  ],
-  'authInteractions': [{
-    'constructor': 'GotoAction',
-    'url': loginUrl,
-    "options": {
-      "externalScripts": [
-        "https://tdcmioiio.herokuapp.com/utils.js",
-        "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
-      ]
-    }
-  },
-  {
-    'constructor': 'CodeAction',
-    'code': authInteractions,
-
-  },
-]
-});
-=======
 if (loginUrl) {
   const authInteractions = `module.exports = ${dir.authInteractions.toString()}`;
->>>>>>> 102d3fd929f6b7ca0fc96f09faaa2837d6a6ee91
 
   var jsonText = JSON.stringify({
     'extractionConfigs': {},
