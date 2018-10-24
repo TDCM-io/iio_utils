447bbf8af058e93c19387ea63f42598a48636343
Daniel Deren <daniel.deren@tdcm.io>
2018-10-24 14:41:57 +0200

    ATrigger & Runscope tests trigger

--- server.js
+++ server.js
@@ -2,15 +2,35 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const axios = require("axios").default;
 const port = process.env.PORT || 9000;
 
-http.createServer(function (req, res) {
-  console.log(`${req.method} ${req.url}`);
+function triggerUrl(res, parsedUrl) {
 
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `./lib${parsedUrl.pathname}`;
+  let searchParams = JSON.parse('{"' + decodeURI(parsedUrl.query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+
+  console.log(searchParams);
+  if (searchParams.type === "runscope") {
+    // GET to trigger runscope test
+    axios({
+      method: 'get',
+      url: `https://api.runscope.com/radar/${searchParams.trigger_token}/trigger?runscope_environment=${searchParams.env_id}`,
+    }).then(resp => {
+      res.statusCode = 200;
+      res.end('URL triggered.');
+    }).catch(err => {
+      console.error(err);
+      res.statusCode = 500;
+      res.end(`${err}`);
+    });
+  } else {
+    res.statusCode = 500;
+    res.end(`Error triggering url: ${parsedUrl.path}.`);
+  }
+}
+
+function provideLibResource(res, pathname) {
+
   // based on the URL path, extract the file extention. e.g. .js, .doc, ...
   const ext = path.parse(pathname).ext;
   // maps file extention to MIME typere
@@ -30,7 +50,7 @@ http.createServer(function (req, res) {
   };
 
   fs.exists(pathname, function (exist) {
-    if(!exist) {
+    if (!exist) {
       // if the file is not found, return 404
       res.statusCode = 404;
       res.end(`File ${pathname} not found!`);
@@ -41,18 +61,31 @@ http.createServer(function (req, res) {
     if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
 
     // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
+    fs.readFile(pathname, function (err, data) {
+      if (err) {
         res.statusCode = 500;
         res.end(`Error getting the file: ${err}.`);
       } else {
         // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.setHeader('Content-type', map[ext] || 'text/plain');
         res.end(data);
       }
     });
   });
+}
 
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  console.log(parsedUrl);
+
+  if (parsedUrl.pathname.indexOf('/trigger') > -1) {
+    triggerUrl(res, parsedUrl);
+  } else {
+    provideLibResource(res, `./lib${parsedUrl.pathname}`);
+  }
 
 }).listen(parseInt(port));
 
